39aabfa045eba777dd43fe7ca2dc0d9b6260fb81
AnastasiaVepreva <145943513+AnastasiaVepreva@users.noreply.github.com>
2023-10-04 22:08:52 +0300

    Update Data preprocessing .py

--- Data preprocessing /Data preprocessing .py
+++ Data preprocessing /Data preprocessing .py
@@ -13,3 +13,15 @@ def delete_fuction(line):
     return line
 
 df['CanonicalizedReaction'] = np.vectorize(delete_fuction)(df['CanonicalizedReaction'])
+
+df['count_reactant'] = df['CanonicalizedReaction'].apply(lambda s: s.count('.')+1)
+print('number of reactants:')
+print(df['count_reactant'].value_counts())
+
+df = df[df['count_reactant']==2]
+df['reactant_1'] = df['CanonicalizedReaction'].apply(lambda s: s.split('.')[0])      
+df['reactant_2'] = df['CanonicalizedReaction'].apply(lambda s: s.split('.')[1].split('>>')[0])
+df['product'] = df['CanonicalizedReaction'].apply(lambda s: s.split('>>')[1])
+df = df[['reactant_1', 'reactant_2', 'product', 'Yield']]
+print(df)
+df.to_csv('final_df.csv')
