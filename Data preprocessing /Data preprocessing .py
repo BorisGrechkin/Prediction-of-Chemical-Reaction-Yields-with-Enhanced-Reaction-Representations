d3ac2e51dadeef58252fc8e6d4f372fe19b94aad
AnastasiaVepreva <145943513+AnastasiaVepreva@users.noreply.github.com>
2023-10-15 15:32:43 +0300

    Update Data preprocessing .py

--- Data preprocessing /Data preprocessing .py
+++ Data preprocessing /Data preprocessing .py
@@ -15,25 +15,25 @@ def delete_fuction(line):
     line = line[:first] + line[last:]
     return line
 
-df['CanonicalizedReaction'] = np.vectorize(delete_fuction)(df['CanonicalizedReaction'])
+df['CanonicalizedReaction'] = np.vectorize(delete_fuction)(df['CanonicalizedReaction']) #create a column where amount of reagents is presented
 
-df['count_reactant'] = df['CanonicalizedReaction'].apply(lambda s: s.count('.')+1)
+df['count_reactant'] = df['CanonicalizedReaction'].apply(lambda s: s.count('.')+1) #print how many reactions contain 1/2/3/4 reagents
 print('number of reactants:')
 print(df['count_reactant'].value_counts())
 
 
-df1 = df[df['count_reactant']==1]
-df1['reactant_1'] = df1['CanonicalizedReaction'].apply(lambda s: s.split('>>')[0])      
+df1 = df[df['count_reactant']==1] #create a dataframe with 1 reagents
+df1['reactant_1'] = df1['CanonicalizedReaction'].apply(lambda s: s.split('>>')[0]) #create a column with reactant   
 df1['reactant_2'] = 0
-df1['product'] = df1['CanonicalizedReaction'].apply(lambda s: s.split('>>')[1])
+df1['product'] = df1['CanonicalizedReaction'].apply(lambda s: s.split('>>')[1]) #create a column with product
 df1= df1[['CanonicalizedReaction', 'reactant_1', 'reactant_2', 'product', 'Yield']]
 
-df2 = df[df['count_reactant']==2]
-df2['reactant_1'] = df2['CanonicalizedReaction'].apply(lambda s: s.split('.')[0])      
-df2['reactant_2'] = df2['CanonicalizedReaction'].apply(lambda s: s.split('.')[1].split('>>')[0])
-df2['product'] = df2['CanonicalizedReaction'].apply(lambda s: s.split('>>')[1])
+df2 = df[df['count_reactant']==2] #create a dataframe with 2 reagents
+df2['reactant_1'] = df2['CanonicalizedReaction'].apply(lambda s: s.split('.')[0]) #create a column with first reactant       
+df2['reactant_2'] = df2['CanonicalizedReaction'].apply(lambda s: s.split('.')[1].split('>>')[0])  #create a column with second reactant 
+df2['product'] = df2['CanonicalizedReaction'].apply(lambda s: s.split('>>')[1])  #create a column with product
 df2 = df2[['CanonicalizedReaction', 'reactant_1', 'reactant_2', 'product', 'Yield']]
 
-df3 = pd.concat([df1, df2], ignore_index=True)
+df3 = pd.concat([df1, df2], ignore_index=True) #merge two dataframes with 1 reactant and 2 reactants
 df3['reactant_2'] = df3['reactant_2'].replace(0, None)
 df3.to_csv('final_dataset3.csv')
