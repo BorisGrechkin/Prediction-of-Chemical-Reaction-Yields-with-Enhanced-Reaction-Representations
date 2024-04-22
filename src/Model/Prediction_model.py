5706b0b14e0c8ebb3f0e3966fb1e35591d546e19
Grechkin.BV <grechkin.bv@avgd.pro>
2024-04-22 19:29:40 +0300

    fix

--- src/Model/Prediction_model.py
+++ src/Model/Prediction_model.py
@@ -1,12 +1,15 @@
-import xgboost as xgb
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_squared_error
+import os
+
 import matplotlib.pyplot as plt
-from yaml import load, Loader
 import numpy as np
-from dvclive import Live
+import pandas as pd
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from yaml import load, Loader
+
+os.chdir('../../')
 
 if __name__ == "__main__":
 
@@ -15,7 +18,7 @@ if __name__ == "__main__":
     target = df[df.columns[-1]].values
     data = df[df.columns[:-1]].values
 
-    with open("configs/train_config.yml", "r") as conf:
+    with open("Configs/train_config.yml", "r") as conf:
         train_config = load(conf, Loader=Loader)["train_config"]
 
     np.random.seed(train_config["seed"])
@@ -27,12 +30,23 @@ if __name__ == "__main__":
 
     xgb_model.fit(X_train, y_train)
     y_pred = xgb_model.predict(X_test)
+    y_pred1 = xgb_model.predict(X_train)
 
     RMSE = mean_squared_error(y_test, y_pred, squared=False)
     R2 = r2_score(y_test, y_pred)
 
-    with Live(save_dvc_exp=True) as live:
-        live.log_params(train_config)
+    fig = plt.figure(figsize=(10, 7))
+    legend = ['Train', 'Test']
+    plt.scatter(y_train, y_pred1, c='purple', s=10)
+    plt.scatter(y_test, y_pred, c='pink', s=10)
+    plt.legend(legend, loc='best')
+    plt.xlabel("Y True")
+    plt.ylabel("Y Predicted")
+    plt.savefig('Vizualization/result.jpg')
+
+
+    #with Live(save_dvc_exp=True) as live:
+        #live.log_params(train_config)
         #live.log_artifact("%s/linear_model.pickle" % os.environ.get("MODELS_PATH"))
-        live.log_metric("train_R2", R2)
-        live.log_metric("test_RMSE", RMSE)
+        #live.log_metric("train_R2", R2)
+        #live.log_metric("test_RMSE", RMSE)
